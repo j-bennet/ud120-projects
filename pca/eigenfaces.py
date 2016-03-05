3293546f06551e14f8ea45e1f2f015f56865b97f
Irina Truong <i.chernyavska@gmail.com>
2016-03-05 13:01:08 -0800

    Lesson 12.

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -66,12 +66,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 150  ## [10, 15, 25, 50, 100, 250]
+
+# [
+# 10, 0.12
+# 15, 0.38
+# 25, 0.64
+# 50, 0.69
+# 100, 0.69
+# 150, 0.55
+# 250, 0.60
+# ]
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
+
+for i in range(2):
+    print 'explained variance: %s : %s' % (i, pca.explained_variance_ratio_[i])
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
@@ -133,14 +146,14 @@ def title(y_pred, y_test, target_names, i):
     true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
     return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
 
-prediction_titles = [title(y_pred, y_test, target_names, i)
-                         for i in range(y_pred.shape[0])]
+# prediction_titles = [title(y_pred, y_test, target_names, i)
+#                          for i in range(y_pred.shape[0])]
 
-plot_gallery(X_test, prediction_titles, h, w)
+# plot_gallery(X_test, prediction_titles, h, w)
 
 # plot the gallery of the most significative eigenfaces
 
-eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
-plot_gallery(eigenfaces, eigenface_titles, h, w)
+# eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+# plot_gallery(eigenfaces, eigenface_titles, h, w)
 
-pl.show()
+# pl.show()
